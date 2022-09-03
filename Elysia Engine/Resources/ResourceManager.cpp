1a5ea767d9ae92af58c0ca347beea61e41f44c3b
Liiii007 <liiii007@outlook.com>
2022-09-04 00:05:32 +0800

    尝试使用filesystem

--- Elysia Engine/Resources/ResourceManager.cpp
+++ Elysia Engine/Resources/ResourceManager.cpp
@@ -2,6 +2,8 @@
 #include <Renderer/Shader.h>
 #include <Tools/Logger.h>
 #include <Resources/MaterialData.h>
+#include <filesystem>
+
 
 void ResourceManager::Init() {
 	Shader::New(L"D:\\Working\\VS Projects\\Elysia Engine\\Elysia Engine\\Renderer\\Shaders\\normal.hlsl", "initShader");
@@ -10,4 +12,10 @@ void ResourceManager::Init() {
 	//Mat data test
 	MaterialData::Load("Green", "D:\\Working\\VS Projects\\Elysia Engine\\Elysia Engine\\Resources\\Materials\\Green.json");
 	MaterialData::Load("Red", "D:\\Working\\VS Projects\\Elysia Engine\\Elysia Engine\\Resources\\Materials\\Red.json");
+
+	//Get all material in directory
+	std::string matPath = "D:\\Working\\VS Projects\\Elysia Engine\\Elysia Engine\\Resources\\Materials";
+	for (const auto& entry : std::filesystem::directory_iterator(matPath)) {
+		auto p = entry.path();
+	}
 }
